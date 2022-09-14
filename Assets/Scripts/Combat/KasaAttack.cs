49bfb70b8e5388128404a7ca241991df43f8fcb4
Colin Charleson <colin.charleson@ontariotechu.net>
2022-09-14 12:03:29 -0400

    Swing Combos

--- Assets/Scripts/Combat/KasaAttack.cs
+++ Assets/Scripts/Combat/KasaAttack.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class KasaAttack : MonoBehaviour
 {
-    public GameObject Kasa;
-    public bool CanAttack = true;
-    public float AttackCooldown = 2.0f;
+    public Animator kasa;
+    public bool canAttack = true;
+    public float attackCooldown = 0.5f;
+
+    public float timeSinceLastHit;
+    public float lastAttack;
 
     private PlayerController movement;
 
@@ -16,9 +19,11 @@ public class KasaAttack : MonoBehaviour
     }
 	void Update()
     {
+        timeSinceLastHit += Time.deltaTime;
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (CanAttack && !movement.onWall && !movement.gliding)
+            if (canAttack && !movement.onWall && !movement.gliding)
             {
                 Attack();
             }
@@ -28,15 +33,40 @@ public class KasaAttack : MonoBehaviour
 
     public void Attack()
     {
-        CanAttack = false;
-        Animator animation = Kasa.GetComponent<Animator>();
-        animation.SetTrigger("Attack");
+        canAttack = false;
+
+        if (timeSinceLastHit > 1.3f)
+		{
+            kasa.SetTrigger("Attack");
+            lastAttack = 1;
+            attackCooldown = 0.5f;
+            
+        }
+        else
+		{
+            if(lastAttack == 2)
+			{
+                kasa.SetTrigger("Attack3");
+                lastAttack = 3;
+                attackCooldown = 2.0f;
+            }
+            if (lastAttack == 1)
+            {
+                kasa.SetTrigger("Attack2");
+                lastAttack = 2;
+                attackCooldown = 0.5f;
+            }
+
+		}
+
+        timeSinceLastHit = 0;
+
         StartCoroutine(AttackCooldownReset());
     }
 
     IEnumerator AttackCooldownReset()
     {
-        yield return new WaitForSeconds(AttackCooldown);
-        CanAttack = true;
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
     }
 }
