df9630bbfcd28df323c85c6b2dc855600bac0de2
Colin Charleson <colin.charleson@ontariotechu.net>
2022-11-10 11:43:03 -0500

    Compass

---
+++ Assets/Scripts/Player/Compass.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+public class Compass : MonoBehaviour
+{
+	public RawImage CompassImage;
+	public Transform Player;
+
+	public void Update()
+	{
+		CompassImage.uvRect = new Rect(Player.localEulerAngles.y / 360, 0, 1, 1);
+	}
+}
