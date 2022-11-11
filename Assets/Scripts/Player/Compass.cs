1d58e24a43f0c05261c5bca1631cd03d768aeba8
Colin Charleson <colin.charleson@ontariotechu.net>
2022-11-10 22:01:44 -0500

    Minor fixes

--- Assets/Scripts/Player/Compass.cs
+++ Assets/Scripts/Player/Compass.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class Compass : MonoBehaviour
 {
 	public RawImage CompassImage;
+	public RawImage WaypointImage;
 	public Transform Player;
+	public Transform Objective;
 
 	public void Update()
 	{
 		CompassImage.uvRect = new Rect(Player.localEulerAngles.y / 360, 0, 1, 1);
+
+		float angle = Vector3.Angle(Player.forward, Objective.position);
+
+		if (Objective)
+			WaypointImage.uvRect = new Rect(angle / 360, 0, 1, 1);
+
+		Debug.Log(angle);
 	}
 }
