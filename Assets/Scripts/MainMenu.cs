d4dba48ca5e10b156989db7ccfa0a6a17f62665e
mark-Toufic <72156370+mark-Toufic@users.noreply.github.com>
2022-11-28 21:39:02 -0500

    Sens changes everywhere

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,8 +7,25 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject resume;
-	private void Update()
+    public Text sensText;
+    public Slider sensSlider;
+
+    public float sens;
+    public GameObject player;
+
+
+
+    void Start()
+    {
+        player.GetComponentInChildren<PlayerController>().mouseSensitivity = sens;
+        sens = PlayerPrefs.GetFloat("CurrentSens", 100);
+        sensSlider.value = sens / 10;
+    }
+    private void Update()
 	{
+       
+
+        PlayerPrefs.SetFloat("CurrentSens", sens );
         string filePath = Application.dataPath + "/save.txt";
         if (System.IO.File.Exists(filePath))
         {
@@ -23,6 +40,14 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
+
+    public void Senestivity(float sensi)
+    {
+        sens = sensi * 10;
+        sensText.text = sens.ToString("F0");
+    }
+
+
 
     public void Quit()
     {
