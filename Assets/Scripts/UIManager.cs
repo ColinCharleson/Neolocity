3311c5f5c633a1330fd06663dfd2ec8587e77960
Suraaj <surdige@gmail.com>
2022-11-30 00:14:20 -0500

    StopFootstepsWhenPaused

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,6 +19,8 @@ public class UIManager : MonoBehaviour
 
     public GameObject load;
 
+    public AudioSource footstepsSource, sprintSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -59,6 +61,8 @@ public class UIManager : MonoBehaviour
 		{
             Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.None;
+            footstepsSource.enabled = false;
+            sprintSource.enabled = false;
         }
 		else
 		{
