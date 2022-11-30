320c4330429810d34d7b1dd6fd29af1c1c508c6b
mark-Toufic <72156370+mark-Toufic@users.noreply.github.com>
2022-11-30 10:37:05 -0500

    music option in main menu removed for now

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,7 +13,17 @@ public class MainMenu : MonoBehaviour
     public Slider fovSlider;
     public float fieldOV;
     public float sens;
+
+    public Text musicText;
+    public Slider musicSlider;
+    public Text ambientText;
+    public Slider ambientlider;
+    public Text sfxText;
+    public Slider sfxSlider;
     public GameObject player;
+ /*   public float sfx, ambient , music;
+   private AudioSource musicSource, sfxSource, ambientSource;
+    private AudioSource walkSource, runSoruce;*/
 
 
     void Start()
@@ -21,15 +31,37 @@ public class MainMenu : MonoBehaviour
         player.GetComponentInChildren<PlayerController>().mouseSensitivity = sens;
         sens = PlayerPrefs.GetFloat("CurrentSens", 100);
         sensSlider.value = sens / 10;
+
         player.GetComponentInChildren<PlayerController>().fov = fieldOV;
         fieldOV = PlayerPrefs.GetFloat("CurrentFov", 60);
         fovSlider.value = fieldOV;
+
+      /*  music = PlayerPrefs.GetFloat("MusicVolume");
+        musicSource.volume = music;
+        musicSlider.value = music;
+
+        ambient = PlayerPrefs.GetFloat("AmbientVolume");
+        ambientSource.volume = ambient;
+        ambientlider.value = ambient;
+
+        sfx = PlayerPrefs.GetFloat("SFXVolume");
+        walkSource.volume = sfx;
+        runSoruce.volume = sfx;
+        sfxSlider.value = sfx;*/
     }
     private void Update()
 	{
-
         PlayerPrefs.SetFloat("CurrentFov", fieldOV);
-        PlayerPrefs.SetFloat("CurrentSens", sens );
+        PlayerPrefs.SetFloat("CurrentSens", sens);
+        /*      PlayerPrefs.SetFloat("MusicVolume", music);
+              musicSource.volume = music;
+
+              PlayerPrefs.SetFloat("AmbientVolume", ambient);
+              ambientSource.volume = ambient;
+
+              PlayerPrefs.SetFloat("SFXVolume", sfx);
+              walkSource.volume = sfx;
+              runSoruce.volume = sfx;*/
 
         string filePath = Application.dataPath + "/save.txt";
         if (System.IO.File.Exists(filePath))
@@ -41,7 +73,24 @@ public class MainMenu : MonoBehaviour
             resume.SetActive(false);
         }
 	}
-	public void PlayGame()
+/*
+    public void MusicVolume(float volume)
+    {
+        music = volume;
+        musicText.text = music.ToString("F0");
+    }
+    public void AmbientVolume(float volume)
+    {
+        ambient = volume;
+        ambientText.text = ambient.ToString("F0");
+    }
+    public void SFXVolume(float volume)
+    {
+        sfx = volume;
+        sfxText.text = sfx.ToString("F0");
+    }*/
+
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
