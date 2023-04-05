1799cf78f977747b828ad55752995bd1db341b57
Colin Charleson <colin.charleson@ontariotechu.net>
2023-04-05 11:08:50 -0400

    Race Updates

--- Assets/RaceTimer.cs
+++ Assets/RaceTimer.cs
@@ -5,65 +5,76 @@ using TMPro;
 
 public class RaceTimer : MonoBehaviour
 {
-    public bool raceStart;
-    private bool raceInit;
-    public float time;
+	public bool raceStart;
+	private bool raceInit;
+	public float time;
 
-    public TextMeshProUGUI timeDisplay;
-    public TextMeshProUGUI winDisplay;
+	public TextMeshProUGUI timeDisplay;
+	public TextMeshProUGUI winDisplay;
 
-    public GameObject player1, player2;
-    public GameObject winner;
-    public Transform startPosition, endPosition;
-    // Update is called once per frame
-    void Update()
-    {
-        if(raceStart)
+	public GameObject player1, player2;
+	public GameObject winner;
+	public Transform startPosition, endPosition;
+	// Update is called once per frame
+	void Update()
+	{
+		if (raceStart)
 		{
-            time += Time.deltaTime;
-            timeDisplay.text = time.ToString("0.00");
+			time += Time.deltaTime;
+			timeDisplay.text = time.ToString("0.00");
 
-        }
-
-        if(!raceInit)
-        if(Vector3.Distance(player1.transform.position, startPosition.position) < 3 && Vector3.Distance(player2.transform.position, startPosition.position) < 3)
-		{
-            raceInit = true;
-            StartRace();
 		}
 
-        if (Vector3.Distance(player1.transform.position, endPosition.position) < 3)
-        {
-            EndRace(player1);
-        }
-        if(Vector3.Distance(player2.transform.position, endPosition.position) < 3)
-		{
-            EndRace(player2);
-		}
-    }
+		if (!raceInit)
+			if (Vector3.Distance(player1.transform.position, startPosition.position) < 3 && Vector3.Distance(player2.transform.position, startPosition.position) < 3)
+			{
+				raceInit = true;
+				StartRace();
+			}
+			else
+			{
+				timeDisplay.text = "Waiting for all players to enter the start zone";
+			}
 
-    public void StartRace()
-	{
-        StartCoroutine(Countdown());
-	} 
-    public void EndRace(GameObject winner)
-	{
-        winDisplay.text = winner.name + " Won The Race";
-        raceStart = false;
+		if (Vector3.Distance(player1.transform.position, endPosition.position) < 3)
+		{
+			EndRace(player1);
+		}
+		if (Vector3.Distance(player2.transform.position, endPosition.position) < 3)
+		{
+			EndRace(player2);
+		}
 	}
 
-    IEnumerator Countdown()
-    {
-        timeDisplay.color = Color.red;
-        timeDisplay.text = "3";
-        yield return new WaitForSeconds(1);
-        timeDisplay.color = Color.yellow;
-        timeDisplay.text = "2";
-        yield return new WaitForSeconds(1);
-        timeDisplay.color = Color.green;
-        timeDisplay.text = "1";
-        yield return new WaitForSeconds(1);
-        timeDisplay.color = Color.white;
-        raceStart = true;
-    }
+	public void StartRace()
+	{
+		StartCoroutine(Countdown());
+	}
+	public void EndRace(GameObject winner)
+	{
+		winDisplay.text = winner.name + " Won The Race";
+		raceStart = false;
+	}
+
+	IEnumerator Countdown()
+	{
+		timeDisplay.color = Color.red;
+		timeDisplay.text = "3";
+		yield return new WaitForSeconds(1);
+		timeDisplay.color = Color.yellow;
+		timeDisplay.text = "2";
+		yield return new WaitForSeconds(1);
+		timeDisplay.color = Color.green;
+		timeDisplay.text = "1";
+		yield return new WaitForSeconds(1);
+		if (Vector3.Distance(player1.transform.position, startPosition.position) < 3 && Vector3.Distance(player2.transform.position, startPosition.position) < 3)
+		{
+			timeDisplay.color = Color.white;
+			raceStart = true;
+		}
+		else
+		{
+			raceInit = false;
+		}
+	}
 }
